ab2c8ded0fe59c69329b757634c4f3b6a2a91def
aliliin <phperali@gmail.com>
2019-02-21 14:52:19 +0800

    Go语言的基本关键字声明

---
+++ src/learn.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// 常量
+const NAME = "Gao"
+
+// 全局变量
+var username = "YongLi"
+
+func main() {
+
+	fmt.Println("my name is")
+	fmt.Print(NAME)
+	fmt.Print(username)
+}
---
+++ src/main.go
@@ -0,0 +1,28 @@
+// 程序所属包 必须所在第一行
+package main
+// 导入依赖包 可以导入多个包
+import "fmt"
+// 定义常量 及 赋值 string 是类型
+const NAME string = "Gao"
+// 全局变量定义 及 赋值 全局变量关键字 var
+var  globalVar string = "global variable"
+// 一般类型声明
+type gaoInt int
+// 结构体的声明
+type Learn struct {
+
+}
+// 声明接口
+type ILearnToday interface{
+
+}
+// 函数定义
+func LearnFunction(){
+	fmt.Println("Today Learn Golang! ")
+}
+// main 函数 相当于入口
+func main() {
+	LearnFunction()
+ fmt.Println("Learn Golang!")
+}
+
