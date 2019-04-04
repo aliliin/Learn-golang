50ed09e0af390eb1198c0f452a4ad7a5f5ed5366
aliliin <phperali@gmail.com>
2019-04-04 15:44:55 +0800

    bdd 测试工具的使用

---
+++ src/learn/ch36/bdd/bdd_spec_test.go
@@ -0,0 +1,23 @@
+package bdd_test
+
+// bdd 测试工具的使用
+// github 地址 ： https://github.com/smartystreets/goconvey.git
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSpec(t *testing.T) {
+
+	// Only pass t into top-level Convey calls
+	Convey("Given some integer with a starting value", t, func() {
+		x := 1
+		Convey("When the integer is incremented", func() {
+			x++
+			Convey("The value should be greater by one", func() {
+				So(x, ShouldEqual, 2)
+			})
+		})
+	})
+}
