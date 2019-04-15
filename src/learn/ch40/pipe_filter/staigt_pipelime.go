a6eb697e2f32ae3494460cec2655be8de75ec533
aliliin <phperali@gmail.com>
2019-04-15 18:07:22 +0800

    Pipe-Fiter 架构模式

--- src/learn/ch40/pipe_filter/staigt_pipelime.go
+++ src/learn/ch40/pipe_filter/staigt_pipelime.go
@@ -1,5 +1,17 @@
 package pipe_filter
 
+// 架构模式。
+// 面向对象的可复用的设计模式
+
+// Pipe-Fiter 架构模式
+/**
+ * 非常适合数据处理及数据分析系统
+ * Filter 封装数据处理的功能
+ * 松耦合 ： Filter 只跟数据（格式） 耦合
+ * Pipe 用于链接 Filter 传递数据或者在异步处理过程中缓冲数据流进程内部同步调用时，pipe 演变为数据在方法调用间传递
+ */
+
+//  Fiter 和组合模式
 func NewStaightPipeline(name string, filters ...Filter) *StraightPipeline {
 	return &StraightPipeline{
 		Name:    name,
--- src/learn/ch40/pipe_filter/straigt_pipelime_test.go
+++ src/learn/ch40/pipe_filter/straigt_pipelime_test.go
@@ -2,6 +2,18 @@ package pipe_filter
 
 import "testing"
 
+// 架构模式。
+// 面向对象的可复用的设计模式
+
+// Pipe-Fiter 架构模式
+/**
+ * 非常适合数据处理及数据分析系统
+ * Filter 封装数据处理的功能
+ * 松耦合 ： Filter 只跟数据（格式） 耦合
+ * Pipe 用于链接 Filter 传递数据或者在异步处理过程中缓冲数据流进程内部同步调用时，pipe 演变为数据在方法调用间传递
+ */
+
+//  Fiter 和组合模式
 func TestStraightPipeline(t *testing.T) {
 	spliter := NewSplitFilter(",")
 	converter := NewToIntFilter()
