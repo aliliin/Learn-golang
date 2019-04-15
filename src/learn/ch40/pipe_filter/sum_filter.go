4973954e68aa5cfae1302454ffe4fb5e32441eda
aliliin <phperali@gmail.com>
2019-04-15 18:07:00 +0800

    Pipe-Fiter 架构模式 计算结果

--- src/learn/ch40/pipe_filter/sum_filter.go
+++ src/learn/ch40/pipe_filter/sum_filter.go
@@ -4,6 +4,18 @@ import (
 	"errors"
 )
 
+// 架构模式。
+// 面向对象的可复用的设计模式
+
+// Pipe-Fiter 架构模式
+/**
+ * 非常适合数据处理及数据分析系统
+ * Filter 封装数据处理的功能
+ * 松耦合 ： Filter 只跟数据（格式） 耦合
+ * Pipe 用于链接 Filter 传递数据或者在异步处理过程中缓冲数据流进程内部同步调用时，pipe 演变为数据在方法调用间传递
+ */
+
+//  求和
 var SumFiterWrongFormatError = errors.New("input data should be string")
 
 type SumFilter struct {
