ea10209397130c430c37da44c73f2eb228418d44
aliliin <phperali@gmail.com>
2019-04-15 18:06:40 +0800

    Pipe-Fiter 架构模式 转为整型数字

--- src/learn/ch40/pipe_filter/to_int_filter.go
+++ src/learn/ch40/pipe_filter/to_int_filter.go
@@ -5,6 +5,18 @@ import (
 	"strconv"
 )
 
+// 架构模式。
+// 面向对象的可复用的设计模式
+
+// Pipe-Fiter 架构模式
+/**
+ * 非常适合数据处理及数据分析系统
+ * Filter 封装数据处理的功能
+ * 松耦合 ： Filter 只跟数据（格式） 耦合
+ * Pipe 用于链接 Filter 传递数据或者在异步处理过程中缓冲数据流进程内部同步调用时，pipe 演变为数据在方法调用间传递
+ */
+
+//  转为整型
 var ToIntFiterWrongFormatError = errors.New("input data should be string")
 
 type ToIntFilter struct {
